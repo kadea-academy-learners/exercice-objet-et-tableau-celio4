4b75b8f8b7f08b541b5d50735fd23f89ba83c2c9
Fofo Celio <tonemail@example.com>
2026-01-14 13:23:11 +0100

    resolution de l'exercice 4

--- 04-show-student-bulletin.js
+++ 04-show-student-bulletin.js
@@ -13,7 +13,7 @@
 //     - Moyenne >= 12 : "Bien"
 //     - Moyenne >= 10 : "Passable"
 //     - Moyenne < 10  : "À revoir"
-// Dans le cas où l'élève n'a pas de notes, la moyenne doit être considérée comme 0 et le commentaire "À revoir".
+// Dans le cas où l'élève n'a pas de notes, la moyenne doit être considérée comme 0 et le commentaire "À revoir"
 function showStudentBulletin(eleves) {
 	
 }
