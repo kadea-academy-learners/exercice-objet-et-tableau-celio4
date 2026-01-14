90f606214669bf29e20e1746b4fbc1850ca44fd0
Fofo Celio <tonemail@example.com>
2026-01-14 13:19:08 +0100

    creation d'un tableau

--- 02-array-average.js
+++ 02-array-average.js
@@ -1,7 +1,7 @@
 // Crée un tableau `notes` contenant 5 nombres
 // Écrire une fonction averageNote avec `notes` comme parametre qui effectue les opérations suivantes :
 // - Calcule la moyenne des notes
-// - retourne un message "Réussi" si la moyenne est >= 10, sinon "Échoué"
+// - retourne un message "Réussi" si la moyenne est >= 10, sinon "Échoué".
 
 function averageNote(notes) {
 	
